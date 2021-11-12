47caa8e1cb3e282a010f31611b9a8d6ec1e034ce
Alex LaValle <93055115+alexanderjlavalle@users.noreply.github.com>
2021-11-12 14:06:54 +0200

    Delete valit_examples.py

--- valit_examples.py
+++
@@ -1,7 +0,0 @@
-examples = [[[],0,800,800,80]]
-examples.append([[[200,200,200],[500,500,200]],800,0,0,800])
-examples.append([[[400,400,300]],50,400,770,400])
-examples.append([[[400,400,300]],50,50,770,770])
-
-# to enter a new example for value iteration use the following format
-# [[[obstacleX, obstacleY, obstacle radius]], initialX, initialY, goalX, goalY]
